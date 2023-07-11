d522496056e7557a6acd06f1a32144641b0b46e7
r2pen2 <joedobbelaar@gmail.com>
2023-07-11 15:57:38 -0400

    Welcome + Emergency Resources

---
+++ SS/api/data.js
@@ -0,0 +1,328 @@
+/**
+ * Enum for navigation drawer item data
+ * @readonly
+ * @const {Object[]}
+ */
+export const AppDrawerItemData = {
+  Welcome: [
+    {
+      type: "h1",
+      text: "Welcome to WPI!",
+    },
+    {
+      type: "h2",
+      text: "Our Mission:",
+    },
+    {
+      type: "p",
+      text: `"WPI transforms lives, turns knowledge into action to confront global challenges, and revolutionizes STEM through distinctive and inclusive education, projects, and research."`,
+    },
+    {
+      type: "h2",
+      text: "Our Values:",
+    },
+    {
+      type: "li",
+      text: "Respect",
+      level: 0
+    },
+    {
+      type: "li",
+      text: "Community",
+      level: 0
+    },
+    {
+      type: "li",
+      text: "Inclusion",
+      level: 0
+    },
+    {
+      type: "li",
+      text: "Innovation",
+      level: 0
+    },
+    {
+      type: "li",
+      text: "Achievement",
+      level: 0
+    },
+    {
+      type: "a",
+      text: "Code Of Conduct",
+      href: "https://www.wpi.edu/offices/dean-students/code-of-conduct",
+    },
+    {
+      type: "a",
+      text: "WPI's Strategic Plan",
+      href: "https://www.wpi.edu/offices/president/wpi-strategic-plan-2021-2026",
+    },
+  ],
+  Emergency: [
+    {
+      type: "h1",
+      text: "Emergency Resources - Who to Contact?",
+    },
+    {
+      type: "h2",
+      text: "WPI Resources",
+    },
+    {
+      type: "h3",
+      text: "WPI Police",
+    },
+    {
+      type: "a",
+      text: "WPI Police",
+      href: "https://www.wpi.edu/offices/police",
+    },
+    {
+      type: "a",
+      text: "Emergency - (508) 831-5555",
+      href: "callto:5088315555",
+    },
+    {
+      type: "a",
+      text: "Non-Emergency - (508) 831-5433",
+      href: "callto:5088315433",
+    },
+    {
+      type: "h3",
+      text: "SDCC",
+    },
+    {
+      type: "a",
+      text: "SDCC",
+      href: "https://www.wpi.edu/offices/student-development-counseling-center",
+    },
+    {
+      type: "a",
+      text: "(508) 831-5540",
+      href: "callto:5088315540",
+    },
+    {
+      type: "a",
+      text: "Concerned about a student?",
+      href: "https://fs28.formsite.com/webteamwpiedu/concerned-about-student/index.html?_gl=1*1qw7e0w*_ga*MTk5NjIyODU2LjE2ODQ3ODA0NTY.*_ga_RE35PKQB7J*MTY4NzIwMDY1Ni4xMTMuMS4xNjg3MjAwOTQyLjU1LjAuMA..",
+    },
+    {
+      type: "h3",
+      text: "Campus Shuttles",
+    },
+    {
+      type: "a",
+      text: "Campus Shuttles",
+      href: "https://www.wpi.edu/student-experience/resources/safety/campus-transportation",
+    },
+    {
+      type: "a",
+      text: "(508) 831-5645",
+      href: "callto:5088315645",
+    },
+    {
+      type: "h3",
+      text: "Health Services",
+    },
+    {
+      type: "a",
+      text: "Health Services",
+      href: "https://www.wpi.edu/student-experience/health-counseling/health-services",
+    },
+    {
+      type: "a",
+      text: "(508) 831-5520",
+      href: "callto:5088315520",
+    },
+    {
+      type: "h3",
+      text: "Title IX",
+    },
+    {
+      type: "a",
+      text: "Title IX",
+      href: "https://www.wpi.edu/offices/title-ix/how-to-report-sexual-misconduct",
+    },
+    {
+      type: "a",
+      text: "(508) 831-6514",
+      href: "callto:5088316514",
+    },
+    {
+      type: "h2",
+      text: "Additional Resources",
+    },
+    {
+      type: "h3",
+      text: "State Police",
+    },
+    {
+      type: "a",
+      text: "(508) 829-8326",
+      href: "callto:5088318326",
+    },
+    {
+      type: "h3",
+      text: "Emergency Number",
+    },
+    {
+      type: "ap",
+      text: "911",
+      href: "callto:911",
+    },
+    {
+      type: "h3",
+      text: "Pathways for Change",
+    },
+    {
+      type: "a",
+      text: "Pathways for Change",
+      href: "https://pathwaysforchange.help/",
+    },
+    {
+      type: "a",
+      text: "(800) 870-5905",
+      href: "callto:8008705905",
+    },
+    {
+      type: "h3",
+      text: "YWCA Daybreak",
+    },
+    {
+      type: "a",
+      text: "YWCA Daybreak",
+      href: "https://ywcacm.org/domestic-violence/",
+    },
+    {
+      type: "a",
+      text: "(800) 775-9030",
+      href: "callto:8007759030",
+    },
+    {
+      type: "h3",
+      text: "Suicide and Crisis Hotline",
+    },
+    {
+      type: "a",
+      text: "Suicide and Crisis Hotline",
+      href: "https://988lifeline.org/?utm_source=google&utm_medium=web&utm_campaign=onebox",
+    },
+    {
+      type: "a",
+      text: "988",
+      href: "callto:988",
+    },
+    {
+      type: "h3",
+      text: "UMass Memorial Medical Center",
+    },
+    {
+      type: "a",
+      text: "(508) 334-1000",
+      href: "callto:5083341000",
+    },
+    {
+      type: "h3",
+      text: "St. Vincent Hospital",
+    },
+    {
+      type: "a",
+      text: "(508) 334-5000",
+      href: "callto:5083345000",
+    },
+  ],
+  Additional_Contacts: [
+    {
+      type: "h1",
+      text: "Additional Contacts/Resources",
+    },
+  ],
+  Technology_And_Library: [
+    {
+      type: "h1",
+      text: "Technology and Library Resources",
+    },
+  ],
+  First_Year: [
+    {
+      type: "h1",
+      text: "First Year At WPI? Here's What to Expect:",
+    },
+  ],
+  Transition: [
+    {
+      type: "h1",
+      text: "College Transition - What’s new at WPI?",
+    },
+  ],
+  Offices_And_Resources: [
+    {
+      type: "h1",
+      text: "Offices and Resources",
+    },
+  ],
+  Academic_Resources: [
+    {
+      type: "h1",
+      text: "Academic Resources",
+    },
+  ],
+  Study_Tips: [
+    {
+      type: "h1",
+      text: "Study Tips and Tricks from Students",
+    },
+  ],
+  Jobs: [
+    {
+      type: "h1",
+      text: "Internships, Jobs, and Fellowships",
+    },
+  ],
+  Health: [
+    {
+      type: "h1",
+      text: "Health and Wellbeing",
+    },
+  ],
+  Housing_Dining_Transportation: [
+    {
+      type: "h1",
+      text: "Housing, Dining, and Transportation",
+    },
+  ],
+  Diversity_And_Inclusion: [
+    {
+      type: "h1",
+      text: "Diversity and Inclusion Organizations",
+    },
+  ],
+  Financial_Resources: [
+    {
+      type: "h1",
+      text: "Financial Resources",
+    },
+  ],
+  Getting_Involved: [
+    {
+      type: "h1",
+      text: "Getting Involved",
+    },
+  ],
+  Exploring_Worcester: [
+    {
+      type: "h1",
+      text: "Exploring Worcester",
+    },
+  ],
+  FAQ: [
+    {
+      type: "h1",
+      text: "Frequently Asked Questions",
+    },
+  ],
+  Glossary: [
+    {
+      type: "h1",
+      text: "Glossary of Terms and Acronyms",
+    },
+  ],
+}
