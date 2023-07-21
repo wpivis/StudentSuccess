71f3bea10cded91b8dcccdb99a112b37c7db6b06
randyer <rbdyer3@gmail.com>
2023-07-21 13:25:11 -0400

    home page done

--- SS/components/Accordion.jsx
+++ SS/components/Accordion.jsx
@@ -1,68 +1,42 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { List } from 'react-native-paper';
-import {
-  MD3LightTheme as DefaultTheme,
-  PaperProvider,
-} from 'react-native-paper';
-
 
 const WPICrimson = "#AC2B37";
 const WPIGray = '#A9B0B7';
 
-const accordionStyle = {
-  backgroundColor: WPICrimson,
-}
-const accordionItemStyle = {
-  backgroundColor: WPICrimson,
-}
-List.Item.style = {accordionItemStyle};
+export function AccordionTitle ({title, children}) {
+  const childElements = React.Children.map(children, child => {
+    return <React.Fragment>{child}</React.Fragment>;
+  });
+  return ( <View>
+  <List.Accordion titleStyle={{ color: 'white' }} style = {styles.heading} title= {title}>
+    {childElements}
+  </List.Accordion>
+</View>
+  )
+};
 
-
-
-export function AccordionList (props) {
-  const [expanded, setExpanded] = React.useState(true);
-  const handlePress = () => setExpanded(!expanded);
-
-  const {name, items} = props.content;
+export function AccordionItem(props){
   return (
-    <View>
-      <List.Accordion titleStyle={{ color: 'white' }} style = {styles.heading}
-        title= {name}>
-        {/* create list items */}
-        {items.map((description) => {
-        return accordionItem(description);
-        })}
-
-      </List.Accordion>
-    </View>
+    <List.Item style={styles.item} title= {props.title} titleStyle={{ color: 'black' }} />
   )
 }
 
-function accordionItem(description){
-  return(
-<List.Item style={styles.item} title= {description} titleStyle={{ color: 'black' }} />
+export function AccordionLink(props){
+  return (
+    <a href={props.href} target="_blank" rel="noopener noreferrer">
+      <List.Item style={styles.item} title={props.title} titleStyle={{ color: 'blue' }} />
+    </a>
   )
 }
 
 const styles = StyleSheet.create({
   heading: {
     backgroundColor: WPICrimson,
+    paddingBottom: '5px',
   },
   item: {
     backgroundColor: WPIGray,
   },
 });
-
-//create function to load accordion
-//props is an array of accordion sections
-export function renderAccordion (props){
-  const accordionArray = props.content;
-  console.log(accordionArray);
-}
-  // render each accordion section
-  function renderAccordionSection(section){
-
-  }
-     //render title
-     //render items
